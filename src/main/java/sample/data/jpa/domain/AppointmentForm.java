569d84e893bf06f1328603a02f00840040839bdc
Curtis K <74824226+KLN05@users.noreply.github.com>
2021-11-29 16:00:21 +0100

    rdv apt form etc

---
+++ src/main/java/sample/data/jpa/domain/AppointmentForm.java
@@ -0,0 +1,99 @@
+package sample.data.jpa.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+
+
+// PAS BESOIN DU FORM SI LISTE DéROULANTE
+public class AppointmentForm {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	@NotNull
+	private String date;
+	
+	//Faudra peut faire un form pour pouvoir faire un new Appointement avec une durée prédéfini
+	//@GeneratedValue(strategy = GenerationType.AUTO)
+	private int lenght;
+	
+	@NotNull
+	@ManyToOne
+	private String us;
+	
+	@NotNull
+	@ManyToOne
+	private String work;
+	
+	private String description;
+	//ajouter un créneau plutot qu'une durée ?
+	
+
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public int getLenght() {
+		return lenght;
+	}
+	
+	public void setLenght(int lenght) {
+		this.lenght = lenght;
+	}
+	
+
+	//@ManyToOne
+	public String getUs() {
+		return us;
+	}
+	
+	public void setUs(String us) {
+		this.us = us;
+	}
+	
+	//@ManyToOne
+	public String getWork() {
+		return work;
+	}
+	
+	public void setWork(String work) {
+		this.work = work;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return String.valueOf(getId())+ " | RDV Le " + getDate() + " de " + getUs() +
+			      " avec " + getWork() + "<br>" + "Motif :" + getDescription() + "<br>";
+	}
+
+}
--- src/main/java/sample/data/jpa/web/IndexController.java
+++ src/main/java/sample/data/jpa/web/IndexController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -23,30 +24,46 @@ import sample.data.jpa.service.UserDao;
 import sample.data.jpa.service.WorkerDao;
 
 @Controller
-public class IndexController {	
-	
+public class IndexController {
+
 	@Autowired
-	private  UserDao userDao;
-	
+	private UserDao userDao;
+
 	@Autowired
-	private  WorkerDao workerDao;
-	
+	private WorkerDao workerDao;
+
 	@Autowired
-	private  AppointmentDao appointmentDao;
-	
+	private AppointmentDao appointmentDao;
+
 	private String welcomeMessage;
 	private String errorMessage;
 
 	private long connectedUser;
-	
+
 	@GetMapping("/")
 	public String index() {
 		return "page1.html";
 	}
-	
+
 	@GetMapping("/rdv")
-	public String rdv() {
-		return "rdv.html";
+	public String rdv(Model model) {
+		Appointment appoint = new Appointment();
+		model.addAttribute("appoint", appoint); 
+		return "rdv";
+	}
+
+	// VOIR LE SOUCIS RECUPéRATION D'UN STRING AU LIEU DU USER/WORKER
+	@PostMapping("/prendreRdv")
+	public String saveRDV(Model model, @ModelAttribute("appoint") Appointment appoint) {
+		String date = appoint.getDate();
+		int length = appoint.getLenght();
+		User us = appoint.getUs();
+		Worker wrk = appoint.getWork();
+		String desc = appoint.getDescription();
+		
+		//if (userDao.findById(us.getId()) == null || workerDao.findById(wrk.getId())==null) {
+			appointmentDao.save(appoint);
+			return "redirect:mypage";
 	}
 	
 	@GetMapping("/deco")
@@ -60,7 +77,7 @@ public class IndexController {
 		model.addAttribute("user", user);
 		return "inscription2.html";
 	}
-	
+
 	@PostMapping("/inscription2")
 	public String saveUser(Model model, @ModelAttribute("user") User user) {
 		String mail = user.getEmail();
@@ -72,14 +89,14 @@ public class IndexController {
 		model.addAttribute("errorMessage", "Wesh pelo, le mail est déjà utilisé");
 		return "inscription2";
 	}
-	
+
 	@GetMapping("/inscription")
 	public String inscP(Model model) {
 		Worker worker = new Worker();
 		model.addAttribute("worker", worker);
 		return "inscription.html";
 	}
-	
+
 	@PostMapping("/inscription")
 	public String saveWorker(Model model, @ModelAttribute("worker") Worker worker) {
 		String mail = worker.getEmail();
@@ -91,19 +108,19 @@ public class IndexController {
 		model.addAttribute("errorMessage", "Wesh pelo, le mail est déjà utilisé");
 		return "inscription";
 	}
-	
+
 	@GetMapping("/prendreRdv")
 	public String prendreRDV(Model model) {
 		Appointment ap = new Appointment();
-		model.addAttribute("ap", ap); 
+		model.addAttribute("ap", ap);
 		return "rdv.html";
 	}
-	
-	@PostMapping("/prendreRdv")
-	public String saveRDV(Model model, @ModelAttribute("appoint") Appointment appoint) {
-			appointmentDao.save(appoint);
-			return "redirect:mypage";
-	}
+
+//	@PostMapping("/prendreRdv")
+//	public String saveRDV(Model model, @ModelAttribute("appoint") Appointment appoint) {
+//		appointmentDao.save(appoint);
+//		return "redirect:mypage";
+//	}
 
 	@GetMapping("/connection")
 	public String connect(Model model) {
@@ -113,57 +130,56 @@ public class IndexController {
 	}
 
 	@PostMapping("/connection")
-    public String connectUser(Model model, @ModelAttribute("user") User user) {
-        String mail = user.getEmail();
-        String mdp = user.getPassword();
-        try {
-            User userTemp = userDao.findByEmail(mail);
-            if (!userTemp.getPassword().equals(mdp)) {
-                model.addAttribute("errorMessage", "Wesh pelo, Mauvais mdp fdp !");
-                return "connection";
-            }
-            connectedUser = userTemp.getId();
-        } catch (Exception ex) {
-            model.addAttribute("errorMessage", "Wesh pelo, le mail est pas dans la base, tu veux t'inscrire ?");
-            return "connection";
-        }
-        return "redirect:mypage";
-    } 
-	
+	public String connectUser(Model model, @ModelAttribute("user") User user) {
+		String mail = user.getEmail();
+		String mdp = user.getPassword();
+		try {
+			User userTemp = userDao.findByEmail(mail);
+			if (!userTemp.getPassword().equals(mdp)) {
+				model.addAttribute("errorMessage", "Wesh pelo, Mauvais mdp fdp !");
+				return "connection";
+			}
+			connectedUser = userTemp.getId();
+		} catch (Exception ex) {
+			model.addAttribute("errorMessage", "Wesh pelo, le mail est pas dans la base, tu veux t'inscrire ?");
+			return "connection";
+		}
+		return "redirect:mypage";
+	}
+
 //	@GetMapping("/mypage")
 //	public String acc(Model model) {
 //		model.addAttribute("users", userDao.findAll());
 //		return "mypage";
 //	}
-	
-	  @RequestMapping("/get-by-user")
-	  @ResponseBody
-	  public String aptByUser(long id) {
-	    try {
-	    	//User us = userDao
-	      Appointment appointment = appointmentDao.findById(id).get();
-	      appointmentDao.delete(appointment);
-	    }
-	    catch (Exception ex) {
-	      return "Error deleting the appointment:" + ex.toString();
-	    }
-	    return "Appointment "+ id +" succesfully deleted!";
-	  }
-	  
-	@GetMapping("/mypage")
-    public String acc(Model model) {
-       // model.addAttribute("reus", aptByUser(connectedUser));		
-		List<Appointment> appaintmentDao = new ArrayList<>();
-		for(Appointment d : appointmentDao.findAll()) {
 
-			if(d.getUs().getId() == connectedUser || d.getWork().getId() == connectedUser) {
+//	@RequestMapping("/get-by-user")
+//	@ResponseBody
+//	public String aptByUser(long id) {
+//		try {
+//			// User us = userDao
+//			Appointment appointment = appointmentDao.findById(id).get();
+//			appointmentDao.delete(appointment);
+//		} catch (Exception ex) {
+//			return "Error deleting the appointment:" + ex.toString();
+//		}
+//		return "Appointment " + id + " succesfully deleted!";
+//	}
+
+	@GetMapping("/mypage")
+	public String acc(Model model) {
+		// model.addAttribute("reus", aptByUser(connectedUser));
+		List<Appointment> appaintmentDao = new ArrayList<>();
+		for (Appointment d : appointmentDao.findAll()) {
+
+			if (d.getUs().getId() == connectedUser || d.getWork().getId() == connectedUser) {
 				appaintmentDao.add(d);
 			}
 		}
-        model.addAttribute("reus", appaintmentDao);
-        return "mypage"; 
-    }
-	
+		model.addAttribute("reus", appaintmentDao);
+		return "mypage";
+	}
+
 //	 @PostMapping("/delete/{id}", method = RequestMethod.POST))
 //	  public String delete(@PathVariable("id") long id) {
 //		 System.out.println("azergthyujk,tygr");
@@ -177,20 +193,19 @@ public class IndexController {
 //	    }
 //	  }
 
-	  //@RequestMapping(value = "/delete/{id}", method = RequestMethod.POST)
-	  @ResponseBody
-	  public String delete(@PathVariable("id") long id) {
-		 System.out.println("azergthyujk,tygr");
-	    try {
-	      Appointment appointment = appointmentDao.findById(id).get();
-	      appointmentDao.delete(appointment);
-	      return "redirect:mypage";
-	    }
-	    catch (Exception ex) {
-	      return "Error deleting the appointment:" + ex.toString();
-	    }
-	  }
-	
-	
+	 @RequestMapping(value = "/delete/{id}", method = { RequestMethod.GET, RequestMethod.POST })
+	//@DeleteMapping("/delete/{id}")
+	@ResponseBody
+	public void delete(@PathVariable("id") long id) {
+		System.out.println("azergthyujk,tygr");
+		try {
+			Appointment appointment = appointmentDao.findById(id).get();
+			appointmentDao.delete(appointment);
+			
+		} catch (Exception ex) {
+			//return "Error deleting the appointment:" + ex.toString();
+		}
+		
+	}
 
 }
